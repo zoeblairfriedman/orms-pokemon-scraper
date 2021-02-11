c4bed3a8d67896548a766a863b1c3ae58d65b980
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-11 15:00:36 -0500

    commit tree 3b3b04c6a13b2267f5bb796383b1697cc910df49

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,13 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+
+# all_pokemon.map do |row|
+#     puts "#{row[1]} // #{row[2]}"
+# end
+
+water_pokemon = @db.execute("SELECT name FROM pokemon WHERE type = 'Water';")
+# puts fire_pokemon
+# puts Pokemon.find(1, @db).name
+Pokemon.save('Chewtle', 'Water', @db)
+puts water_pokemon
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
+require 'pry'
+
 class Pokemon
+attr_accessor :name, :type, :db
+attr_reader :id
+
+def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+end
+
+def self.save(name, type, db)
+    sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES(?, ?)
+    SQL
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+end
+
+
+def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ?"
+    result = db.execute(sql, id)[0]
+    Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+end
+
+
+
 end
